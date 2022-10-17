ee629349d45df20255d3f787e1a4e0943bf21741
dannyonin <dannyonin@gmail.com>
2022-10-18 01:39:03 +0300

    get json from TMDB data

--- PosterAPI/My_TMDB_App.py
+++ PosterAPI/My_TMDB_App.py
@@ -1,3 +1,20 @@
+'''
+
+     MY API PROGRAM WITH FOR MOVIE POSTERS
+     *SEARCH FOR POSTER IN TMDB DATA BASE
+     *UPLOAD THE DATA TO MONGODB  WITH FLASK
+     *ABILITY TO SEARCH IN THE DATA BASE
+
+
+    <the code is not in final version>
+    <work in progress>
+
+    Author: Daniel Yonin
+    AWSRestart 2022
+
+'''
+
+
 import tmdbsimple as tmdb
 from flask import Flask
 from flask_mongoengine import MongoEngine
@@ -7,7 +24,7 @@ import requests
 import json
 
 
-
+# Class Movie built with TMDB API
 class Movie():
     import tmdbsimple as tmdb
     import os
@@ -15,38 +32,25 @@ class Movie():
     def __init__(self):
 
         self.tmdb.API_KEY = self.os.environ["API_KEY"]
-        self.movie_title = str
+        self.movie_title = input("Please Enter name of a Movie:")
         self.movie_title_found = str
         self.movie_atr = {}
-        self.movie_result = {self.movie_title_found: [self.movie_atr]}
-        self.movie_title_list = []
+        self.movie_result = {}
 
-
-
-
+    # Search Function for movie
     def search(self):
-        # tmdb.REQUESTS_SESSION = requests.Session()
-
         search = tmdb.Search()
         search = search.movie(query=f'{self.movie_title}')
+
         for info in search['results']:
-            self.movie_title_list.append(info['original_title'])
             self.movie_atr = {key:value for (key,value) in info.items()}
-            self.movie_result = {title:atr for (title,atr) in zip(self.movie_title_list,self.movie_atr.items())}
-            print((self.movie_atr))
-        print((self.movie_result))
+            self.movie_title_found = info['original_title']
+            self.movie_result[self.movie_title_found] = self.movie_atr
 
-
-                # print(self.movie_result)
-
-            # print(info['title'], info['id'], info['release_date'], info['original_language'],info['popularity'],info['poster_path'])
-
-
-
-
+        return json.dumps(self.movie_result, indent=3)
 
 
 movie= Movie()
-movie.movie_title = 'Avengers'
-print(movie.movie_title)
-movie.search()
+a=movie.movie_title
+print(a)
+print(movie.search())
