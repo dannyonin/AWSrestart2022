2f10b9195fa51e4ef974b20f03ab19a9683cd75e
dannyonin <dannyonin@gmail.com>
2022-10-18 01:06:39 +0300

    changes at __init__ and search

--- PosterAPI/My_TMDB_App.py
+++ PosterAPI/My_TMDB_App.py
@@ -3,29 +3,43 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 from bs4 import BeautifulSoup
 import requests
-import os
+# import os
 import json
 
 
 
 class Movie():
+    import tmdbsimple as tmdb
+    import os
+
     def __init__(self):
+
+        self.tmdb.API_KEY = self.os.environ["API_KEY"]
         self.movie_title = str
-        self.movie_atr = {self.movie_title: []}
+        self.movie_title_found = str
+        self.movie_atr = {}
+        self.movie_result = {self.movie_title_found: [self.movie_atr]}
+        self.movie_title_list = []
+
+
+
 
     def search(self):
-        tmdb.API_KEY = os.environ["API_KEY"]
         # tmdb.REQUESTS_SESSION = requests.Session()
 
         search = tmdb.Search()
-        response = search.movie(query=f'{self.movie_title}')
-        for info in search.results:
-            for keys in info:
-                movie_result = {}
-                atributes = keys
-            print(info)
+        search = search.movie(query=f'{self.movie_title}')
+        for info in search['results']:
+            self.movie_title_list.append(info['original_title'])
+            self.movie_atr = {key:value for (key,value) in info.items()}
+            self.movie_result = {title:atr for (title,atr) in zip(self.movie_title_list,self.movie_atr.items())}
+            print((self.movie_atr))
+        print((self.movie_result))
 
-            print(info['title'], info['id'], info['release_date'], info['original_language'],info['popularity'],info['poster_path'])
+
+                # print(self.movie_result)
+
+            # print(info['title'], info['id'], info['release_date'], info['original_language'],info['popularity'],info['poster_path'])
 
 
 
