6f1de135602dd369bf681269e35286ca5acab0c0
dannyonin <dannyonin@gmail.com>
2022-10-19 01:44:43 +0300

    fix movies/<id> page for data view and changed the search keyword to harry potter

--- PosterAPI/Mongo_data_movies.py
+++ PosterAPI/Mongo_data_movies.py
@@ -3,6 +3,7 @@ from flask_mongoengine import MongoEngine
 from mongoengine import *
 from My_TMDB_App import Movie
 import json
+from titlecase import titlecase
 
 app = Flask(__name__)
 
@@ -30,17 +31,25 @@ class Movie_DB(db.Document):
 
 @app.route('/movies')
 def get_movies():
-    my_movie = Movie()
-    user_choice = my_movie.movie_title
+    my_movies = Movie()
+    user_choice = my_movies.movie_title
     print("Your search <----->", user_choice)
-    movies = my_movie.search()
+    movies = my_movies.search()
     return json.dumps(movies, indent=3), 200
 
 
 @app.route('/movies/<id>')
 def get_one_movie(id: str):
-    movie = Movie.objects(id=id).first()
-    return jsonify(movie), 200
+
+    my_movies = Movie()
+    user_choice = my_movies.movie_title
+    print("Your search <----->", user_choice)
+
+    movies = json.loads(my_movies.search())
+    result = my_movies.menu()
+    print("data page from movie title",id)
+
+    return  movies[titlecase(id)], 200
 
 
 
