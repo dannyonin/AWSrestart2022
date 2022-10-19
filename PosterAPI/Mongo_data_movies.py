d61e8173484ae05a8136676982ab8ebb9b740afa
dannyonin <dannyonin@gmail.com>
2022-10-19 19:05:45 +0300

    some tests

--- PosterAPI/Mongo_data_movies.py
+++ PosterAPI/Mongo_data_movies.py
@@ -10,22 +10,27 @@ from gridfs import GridFS
 app = Flask(__name__)
 
 
-class Image_Database():
+class Database():
 	def __init__(self):
+		self.dataframe = {}
 		self.mongo_host = "localhost"
 		self.mongo_port = 27017
-		self.mongo_db = 'MyPostersDB'
-		self.mongo_collection = 'movies'
+		self.mongo_db = 'MY_Movies_Database'
+		self.mongo_collection = 'Movies'
 		self.mongo_safe = False
 		self.conn=connect(self.mongo_host, self.mongo_port)
 		self.db=self.conn[self.mongo_db]
 		self.collection = self.db[self.mongo_collection]
 		self.grid = GridFS(self.db, self.mongo_collection+".fs")
 
-	def drop(self):
-		self.db.drop_collection(self.mongo_collection)
-		self.db.drop_collection(self.mongo_collection+".fs.chunks")
-		self.db.drop_collection(self.mongo_collection+".fs.files")
+	def craete_collection(self):
+		pass
+
+
+	def drops(self):
+		self.db.collection.drop(self.mongo_collection)
+		self.db.collection.drop(self.mongo_collection+".fs.chunks")
+		self.db.collection.drop(self.mongo_collection+".fs.files")
 
 	def set_raw(self, key, value):
 		try:
@@ -33,6 +38,14 @@ class Image_Database():
 		except InvalidStringData:
 			self.rm(key)
 			self.grid.put(value, _id=key)
+
+	def mongodb_connect(self):
+
+		try:
+			return pymongo.MongoClient(self.mongo_host, self.mongo_port)
+
+		except pymongo.errors.ConnectionFailure:
+			print("Failed to connect to server {}").format(self.mongo_host, self.mongo_port)
 
 
 class Movie_info(DynamicDocument):
@@ -57,7 +70,7 @@ def get_movies():
     print("Your search <----->", user_choice)
     all_movies = get_movies.search()
 
-    return json.dumps(all_movies, indent=3), 200
+    return json.loads(all_movies), 200
 
 
 @app.route('/genres')
@@ -66,22 +79,39 @@ def list_genres():
 	genres = Movie()
 	my_genre_list = genres.genre_list()
 
-	return json.dumps(my_genre_list) ,200
+	return json.loads(my_genre_list) ,200
 
-@app.route('/movies/<id>')
-def get_one_movie(id: str):
+@app.route('/movies/<movie_name>')
+def get_one_movie(movie_name: str):
 
     my_movies = Movie()
     user_choice = my_movies.movie_title
     print("Your search <----->", user_choice)
     movies = json.loads(my_movies.search())
     result = my_movies.menu()
-    print("data page from movie title",id)
+    print("data page from movie title",movie_name)
 
-    return  movies[titlecase(id)], 200
+    return  movies[titlecase(movie_name)], 200
+
+print()
+
+# my_data_base = Database()
+# print(my_data_base.collection)
+# data = json.dumps(get_one_movie('cars 2')[0])
+# print(data)
+# print(type(data))
+# jsonData = json.loads(data)
+# print(type(jsonData))
+# for x in jsonData:
+# 	print(x)
 
 
-my_mongo = Image_Database()
-drop = my_mongo.drop()
+
+# movie_1 =my_data_base.collection.insert_one(dict(get_one_movie("cars 3")[0]))
+
+
+
+
+
 
 app.run(debug=True)
